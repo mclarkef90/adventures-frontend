4bf73d62b53340281e2c574e949241f10ea45858
morgancfarrow90 <63519344+morgancfarrow90@users.noreply.github.com>
2020-08-25 10:05:12 -0400

    created MyApp component, tested connection to backend with fetch

---
+++ src/MyApp.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default class MyApp extends React.Component {
+
+  componentDidMount(){
+    fetch('http://localhost:3000/api/v1/users/1/comments')
+    .then(response => response.json())
+    .then(data => console.log(data))
+  }
+
+  render(){
+    return(
+      <div>
+      Hi
+      </div>
+    )
+  }
+}
