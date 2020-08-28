1fa328245d5c7e47a45b098e066d3b8d1d6445a9
morgancfarrow90 <63519344+morgancfarrow90@users.noreply.github.com>
2020-08-28 13:29:20 -0400

    changed edit adventure

--- src/components/AdventuresList.js
+++ src/components/AdventuresList.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import {Route, Link} from 'react-router-dom';
+import {Route, Link, Switch} from 'react-router-dom';
 import Adventure from './Adventure'
+import EditAdventure0 from './EditAdventure0'
 
 
 export default function AdventuresList(props){
@@ -9,10 +10,19 @@ export default function AdventuresList(props){
     {console.log(props)}
     <h2> Adventures </h2>
     {console.log(props)}
-    {props.adventures.map(adventure => <span key={adventure.id}> <Link to={`/adventures/${adventure.id}`}>{adventure.title}  </Link><br/>
-    <Link to={`/adventures/${adventure.id}/edit`}> Edit Adventure </Link><br/><br/>
-   </span>
-     )}
+
+    {props.adventures.map(adventure => <span key={adventure.id}> <Link to={`/adventures/${adventure.id}`}>{adventure.title}  </Link> <br/>
+    <Link to={`/users/${adventure.user_id}/adventures/${adventure.id}/edit`}> Edit Adventure </Link> <br/><br/>
+
+    <Switch>
+    <Route path="/users/:id/adventures/:adventure_id/edit" render={(routerProps) => <EditAdventure0 {...routerProps} adventure={adventure}/>} />
+
+    </Switch>
+
+   </span>)}
+
+
+
     </div>
       )
     }
