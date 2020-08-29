1b30ccbdc5a2ca06e7d068a858cc1fd3a9b30ad9
morgancfarrow90 <63519344+morgancfarrow90@users.noreply.github.com>
2020-08-29 16:22:48 -0400

    added search page

--- src/components/AdventureSearch.js
+++ src/components/AdventureSearch.js
@@ -6,13 +6,33 @@ class AdventureSearch extends React.Component{
   constructor(props){
     super(props)
     console.log(this.props)
+    this.state={
+      searchTerm: ""
+    }
+  }
+
+  searchChangeHandler = event => {
+    this.setState({
+      searchTerm: event.target.value
+    })
   }
 
   render(){
+    let results= this.props.adventures.filter((adventure) => adventure.description.toLowerCase().indexOf(this.state.searchTerm.toLowerCase()) !== -1)
+
     return(
       <div>
       <br/>
-      hi
+      <input type="text" value={this.state.searchTerm} placeholder="Search Adventures" name="searchTerm" onChange={this.searchChangeHandler}/>
+      <br/>
+
+
+      {results.map(adventure =>
+        <ul key={adventure.id}>
+        <img className="image_thumbnail" alt="adventure" src={adventure.image_url}/>
+        <h1>{adventure.title}</h1>
+        <p>{adventure.description}</p>
+        </ul>)}
 
       </div>
     )}
